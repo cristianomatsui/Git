3bef849bd22fe9cedc60ce669220f2465c9b68e6
Cristiano <cristiano.matsui@gmail.com>
2016-08-20 14:20:53 -0300

    Project Folder

---
+++ PO28CP201602/TSP DRAFT GRASP/TSP_GRASP.py
@@ -0,0 +1,27 @@
+#def read_file(instance, num, tipo):
+	
+print("Informe o nome da instancia")
+instance = input()
+	
+file = open(instance, 'r')
+print('File opened')
+
+line = file.readline()
+	
+while (line != 'Distance'):
+        
+    if 'N: ' in line:
+        (aux, num_Nodes) = line.split(" ")
+        num_Nodes = int(num_Nodes)
+        num = num_Nodes
+        print(num)
+        
+    if 'edgeWeightType: ' in line:
+        (aux, weightType) = line.split(" ")      
+        weightType = weightType.replace('\n', '').replace('\r', '')     
+        tipo = weightType
+        print(tipo)
+            
+    print("final")        
+    line = file.readline()
+
